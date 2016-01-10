c0d2f2ed31ef91902d198686b7c3776b98d19727
Nitesh Oswal <nit.oswal@gmail.com>
2016-01-11 01:46:18 +0530

    Major updates, add content, and finish out the V1 layout

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,27 @@
 ;(function() {
 
     $(document).ready(function() {
-        $("#wedding-container").fullpage();
+        $("body").flowtype();
+        $("#wedding-container").fullpage({
+            navigation: true,
+            lockanchors: true,
+            navigationTooltips: ['Big NEWS!', 'Meet the Couple', 'Dates & Venue', 'Countdown & Contact'],
+            responsiveWidth: 920
+        });
+        var currentDate = new Date();
+
+        // Set some date in the future. In this case, it's always Jan 1
+        var futureDate  = new Date('02-04-2016');
+
+        // Calculate the difference in seconds between the future and current date
+        var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
+
+        // Instantiate a coutdown FlipClock
+        clock = $('#countdown').FlipClock(diff, {
+            clockFace: 'DailyCounter',
+            countdown: true,
+            showSeconds: false
+        });
     });
 
 })();
